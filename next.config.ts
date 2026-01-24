bfff2ea7b97c9075b4b7da1937e3447576f2e6ba
nishchay-v <nishchayvashistha@gmail.com>
2026-01-24 08:23:45 +0000

    [fix] Adjust remotePatterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL(`${process.env.R2_PUBLIC_URL}/**` || "https://assets.nishchay.dpdns.org")],
+    remotePatterns: [
+      new URL(
+        process.env
+          ? `${process.env.R2_PUBLIC_URL}/**`
+          : "https://assets.nishchay.dpdns.org/**",
+      ),
+    ],
   },
 };
 
