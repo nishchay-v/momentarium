3a17451b414c5b416244ad3d0da9eaa5c11bc074
nishchay-v <nishchayvashistha@gmail.com>
2026-01-26 07:50:31 +0000

    [refactor] Update remotePatterns in next.config.ts to use URL object for improved clarity

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
+const r2Url = process.env.R2_PUBLIC_URL || "https://assets.nishchay.dpdns.org";
+const url = new URL(r2Url);
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      new URL(
-        process.env.R2_PUBLIC_URL
-          ? `${process.env.R2_PUBLIC_URL}/**`
-          : "https://assets.nishchay.dpdns.org/**",
-      ),
+      {
+        protocol: url.protocol.replace(':', '') as 'http' | 'https',
+        hostname: url.hostname,
+        port: url.port,
+        pathname: '/**',
+      },
     ],
   },
 };
